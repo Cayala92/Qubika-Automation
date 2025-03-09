1d386bdfbd649cf395254c371efa586c2dd12814
Cristian Ayala <cristian.ayalap92@gmail.com>
2025-03-09 08:05:43 -0500

    Initial project Structure

---
+++ pages/landingPage/landingPage.ts
@@ -0,0 +1,18 @@
+
+/**
+ * TODO:
+ * The Landing Page Page Object encapsulates all interactions with elements 
+ * on the landing page of the application. The goal is to make tests more 
+ * maintainable and scalable by separating the page interactions from the test logic.
+ * 
+ * This file contains:
+ * - Selectors for key elements of the landing page.
+ * - Methods to interact with those elements (clicks, checks, etc.).
+ * 
+ * The Page Object pattern allows the same class to be reused across multiple 
+ * tests without duplicating interaction code.
+ */
+
+export class LandingPage {
+
+}
---
+++ tests/landingPage.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+import { LandingPage } from '../pages/landingPage/landingPage'; // Import the Landing Page Page Object
+
+/**
+ * TODO:
+ * This E2E test file verifies the overall behavior of the Landing Page 
+ * through interactions defined in the Page Object. 
+ * 
+ * The file will performs (end-to-end) tests, ensuring that key features 
+ * on the page are working  as expected from the user's perspective.
+ * 
+ * By using the Page Object, interactions with the page are kept separate from 
+ * the tests, making the code cleaner, more scalable, and easier to maintain as 
+ * the project grows.
+ */
+
+test.describe('Landing Page E2E Tests', () => {
+
+});
